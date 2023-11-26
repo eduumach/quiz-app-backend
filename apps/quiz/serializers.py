a96586cba1c736d94bc5b8f40a9481477eb391fe
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2023-11-26 18:22:34 -0300

    Volta a paginacao com 50 um dia eu arrumo isso

--- apps/quiz/serializers.py
+++ apps/quiz/serializers.py
@@ -5,15 +5,11 @@ from apps.quiz.models import Quiz, Category, Difficulty, Type
 
 class QuizQuestionSerializer(serializers.ModelSerializer):
     incorrect_answers = serializers.ListSerializer(child=serializers.CharField(source='incorrect_answer'))
-    length = serializers.SerializerMethodField()
-
-    def get_length(self, obj):
-        return obj.incorrect_answers.count() + 1
 
     class Meta:
         model = Quiz
         fields = ['category', 'type', 'difficulty',
-                  'question', 'correct_answer', 'incorrect_answers', 'length']
+                  'question', 'correct_answer', 'incorrect_answers']
 
 
 class CategorySerializer(serializers.ModelSerializer):
