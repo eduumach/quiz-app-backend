29b260fe87faba17fb02a1fb4b2d21e33c98edad
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2023-11-26 18:20:16 -0300

    Adicionando length

--- apps/quiz/serializers.py
+++ apps/quiz/serializers.py
@@ -5,11 +5,15 @@ from apps.quiz.models import Quiz, Category, Difficulty, Type
 
 class QuizQuestionSerializer(serializers.ModelSerializer):
     incorrect_answers = serializers.ListSerializer(child=serializers.CharField(source='incorrect_answer'))
+    length = serializers.SerializerMethodField()
+
+    def get_length(self, obj):
+        return obj.incorrect_answers.count() + 1
 
     class Meta:
         model = Quiz
         fields = ['category', 'type', 'difficulty',
-                  'question', 'correct_answer', 'incorrect_answers']
+                  'question', 'correct_answer', 'incorrect_answers', 'length']
 
 
 class CategorySerializer(serializers.ModelSerializer):
